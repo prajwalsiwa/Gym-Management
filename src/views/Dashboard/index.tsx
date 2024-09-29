38dd2e7a1faa71559489b78f5fcfb3dc36c4e4ba
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-09-29 23:06:13 +0545

    feat: include sidebar in dashboard

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,3 +1,9 @@
+import SideBar from '@Components/SideBar/SideBar';
+
 export default function Dashboard() {
-  return <h1 className="text-red-700">Dashboard</h1>;
+  return (
+    <h1 className="text-red-700">
+      <SideBar />
+    </h1>
+  );
 }
