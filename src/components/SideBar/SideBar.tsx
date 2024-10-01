01a95648699db0eaecb46913107d1f56c756195e
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-01 17:31:56 +0545

    feat: include sidebaaritem

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,3 +1,14 @@
+import { useTypedSelector } from '@Store/hooks';
+import SideBarItem from './SideBarItem';
+
 export default function SideBar() {
-  return <div className="w-64 border border-black">SideBar</div>;
+  const sidebarList = useTypedSelector(state => state.sidebarSlice.sidebarList);
+
+  return (
+    <div className="flex h-full w-72  flex-col bg-white">
+      {sidebarList.map(item => (
+        <SideBarItem key={item.id} linkName={item.name} path={item.path} />
+      ))}
+    </div>
+  );
 }
