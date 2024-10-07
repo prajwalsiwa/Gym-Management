37e4b1d72d2cf7c9defeb2aefcb3962d53e1af79
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-07 09:39:40 +0545

    feat: include style for active links

--- src/components/SideBar/SideBarItem.tsx
+++ src/components/SideBar/SideBarItem.tsx
@@ -1,18 +1,47 @@
 import Icon from '@Components/common/Icon';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { setActiveItem } from '@Store/actions/sidebar';
 
 interface sideBarPropsTypes {
+  id: number;
   linkName: string;
   path: string;
   iconName: string;
+  isActive: boolean;
 }
 
-function SideBarItem({ linkName, path, iconName }: sideBarPropsTypes) {
+function SideBarItem({
+  id,
+  linkName,
+  path,
+  iconName,
+  isActive,
+}: sideBarPropsTypes) {
+  const dispatch = useDispatch();
+
+  function handleActiveItem(itemId: number) {
+    dispatch(setActiveItem(itemId));
+  }
+
   return (
-    <div className=" flex w-full  pl-8  ">
-      <NavLink to={path} className="flex  items-center justify-center gap-2">
-        <Icon name={iconName} />
-        <div className="text-lg font-normal">{linkName}</div>
+    <div className="flex w-full pl-8">
+      <NavLink to={path} className="w-full">
+        <button
+          type="button"
+          className="flex w-full items-center justify-center gap-3 pr-4"
+          onClick={() => handleActiveItem(id)}
+        >
+          <Icon
+            name={iconName}
+            className={`${isActive ? '!text-2xl !font-medium text-blue-900' : '!text-slate-500'}`}
+          />
+          <div
+            className={`flex w-full items-start  ${isActive ? 'rounded-sm border-r-4 border-r-blue-800 text-lg font-medium tracking-wide text-black' : 'border-none text-lg text-slate-500'}`}
+          >
+            {linkName}
+          </div>
+        </button>
       </NavLink>
     </div>
   );
