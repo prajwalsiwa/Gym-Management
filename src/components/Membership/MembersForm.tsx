59eea430093118744dc65f204628620c4f19b137
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-08 14:12:05 +0545

    feat: create members form component

---
+++ src/components/Membership/MembersForm.tsx
@@ -0,0 +1,9 @@
+function MembersForm() {
+  return (
+    <div className="flex h-full w-full items-center justify-center rounded-2xl  bg-white ">
+      MembersForm
+    </div>
+  );
+}
+
+export default MembersForm;
