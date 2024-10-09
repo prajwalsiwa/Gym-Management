e4df37a9f0af18dad02111ff46eef1a8503aed96
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-09 10:26:52 +0545

    feat: divide membership in grid

--- src/views/Membership/index.tsx
+++ src/views/Membership/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Membership() {
   return (
-    <div className="grid h-full w-full grid-cols-2 gap-4 p-8">
+    <div className="grid h-full w-full grid-cols-[60%_40%] gap-4 p-8">
       <MembersTable />
       <MembersForm />
     </div>
