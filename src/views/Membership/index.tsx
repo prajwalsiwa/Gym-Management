e34d3e13edd4ef97542712702903fd433a6f8c98
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-14 08:22:21 +0545

    feat: add navigation

--- src/views/Membership/index.tsx
+++ src/views/Membership/index.tsx
@@ -2,15 +2,18 @@ import { Label, Select } from '@Components/common/FormUI';
 import SearchInput from '@Components/common/FormUI/SearchInput';
 import MembersTable from '@Components/Membership/MembersTable/MembersTable';
 import { Button } from '@Components/RadixComponents/Button';
+import { useNavigate } from 'react-router-dom';
 
 function Membership() {
+  const navigate = useNavigate();
+
   return (
     <div className=" flex h-full w-full flex-col gap-8 px-10 py-8">
       <div className="membership-header flex w-full items-center justify-between ">
         <div className="flex justify-start">
           <h3>Membership</h3>
         </div>
-        <div className="flex w-full justify-center gap-4">
+        <div className="flex w-full  justify-center gap-4">
           <div className=" flex items-center justify-center gap-2">
             <Label>View By</Label>
             <Select
@@ -23,7 +26,7 @@ function Membership() {
             inputValue="search"
           />
         </div>
-        <div className="add-section flex justify-end gap-4">
+        <div className="add-section flex  justify-end gap-4">
           <Button
             className="h-10 w-40 bg-blue-500 tracking-wide hover:bg-blue-700"
             iconClassname="!text-icon-md !font_semibold"
@@ -35,6 +38,7 @@ function Membership() {
             className="h-10 w-40 bg-blue-500 hover:bg-blue-700"
             iconClassname="!text-icon-md !font_semibold"
             leftIcon="add"
+            onClick={() => navigate('/add-membership')}
           >
             Add New
           </Button>
