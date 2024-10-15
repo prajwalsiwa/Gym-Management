dd9aff52c49f2952c39e8be32d0ded4dd17a8062
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-15 12:54:52 +0545

    feat: add react quill and remove vat

--- src/components/Subscription/SubscriptionForm.tsx
+++ src/components/Subscription/SubscriptionForm.tsx
@@ -1,11 +1,26 @@
 import { Input, Label } from '@Components/common/FormUI';
 import { Button } from '@Components/RadixComponents/Button';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import the styles for the editor
 
 function SubscriptionForm() {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [description, setDescription] = useState(''); // State to handle the description
+
+  const handleDateChange = (date: Date | null) => {
+    setStartDate(date);
+  };
+
+  const handleDescriptionChange = (value: string) => {
+    setDescription(value);
+  };
+
   return (
-    <div className="flex h-full w-full flex-col items-center rounded-2xl   bg-[#f2f2f2]  ">
-      <div className="flex w-full flex-col   px-10 py-10">
+    <div className="flex h-full w-full flex-col items-center rounded-2xl bg-[#f2f2f2]">
+      <div className="flex w-full flex-col px-10 py-10">
         <h2 className="text-xl md:text-2xl">Subscription Package</h2>
         <span className="text-md text-gray-400">Enter package details!</span>
       </div>
@@ -17,8 +32,25 @@ function SubscriptionForm() {
             </Label>
             <Input
               type="text"
-              className="w-full  rounded-lg border"
+              className="w-full rounded-lg border bg-white"
               placeholder="Prajwal"
+            />
+          </div>
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <div className="flex w-full flex-col">
+            <Label>
+              Membership Duration
+              <span className="text-lg text-blue-700">*</span>
+            </Label>
+            <DatePicker
+              showIcon
+              selected={startDate}
+              onChange={handleDateChange}
+              placeholderText="Select Date"
+              dateFormat="yyyy/MM/dd"
+              calendarIconClassName="mt-1"
+              className="h-10 w-full rounded-lg   border border-gray-300 outline-blue-500 focus:outline focus:outline-offset-0"
             />
           </div>
         </div>
@@ -29,7 +61,7 @@ function SubscriptionForm() {
             </Label>
             <Input
               type="number"
-              className="w-full  rounded-lg border"
+              className="w-full rounded-lg border bg-white"
               placeholder="Siwa"
             />
           </div>
@@ -39,7 +71,7 @@ function SubscriptionForm() {
             </Label>
             <Input
               type="number"
-              className="w-full  rounded-lg border"
+              className="w-full rounded-lg border bg-white"
               placeholder="Siwa"
             />
           </div>
@@ -47,40 +79,29 @@ function SubscriptionForm() {
         <div className="flex w-full gap-2">
           <div className="w-full">
             <Label>
-              Vat %<span className="text-lg text-blue-700">*</span>
-            </Label>
-            <Input
-              type="number"
-              className="w-full  rounded-lg border"
-              placeholder="Siwa"
-            />
-          </div>
-          <div className="w-full">
-            <Label>
               Total Price<span className="text-lg text-blue-700">*</span>
             </Label>
             <Input
               type="number"
-              className="w-full  rounded-lg border"
+              className="w-full rounded-lg border bg-white"
               placeholder="Siwa"
             />
           </div>
         </div>
 
-        <div className="relative flex w-full flex-col  items-start justify-start gap-1 ">
-          <Label htmlFor="Upload Photo">
-            Upload Photo <span className="text-lg text-blue-700">*</span>
+        <div className="flex w-full flex-col gap-2">
+          <Label>
+            Description<span className="text-lg text-blue-700">*</span>
           </Label>
-          <Input
-            type="file"
-            // accept="image/*"
-            // onChange={handleFileChange}
-            className=" mb-2 w-full rounded-md border pt-2"
+          <ReactQuill
+            value={description}
+            onChange={handleDescriptionChange}
+            theme="snow" // Using the 'snow' theme for a clean appearance
+            className="scrollbar h-40 max-h-40 w-full overflow-hidden rounded-lg  border  bg-white"
           />
         </div>
-        <div className="description">description</div>
 
-        <div className="mt-4 flex h-full w-full  justify-end gap-4 ">
+        <div className="mt-4 flex h-full w-full justify-end gap-4">
           <Button className="danger h-[2.8rem] w-full bg-rose-600 hover:bg-rose-700">
             Clear Form
           </Button>
