e2e4423d730181ecba71675ae40cfb475fd79d13
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-15 12:59:57 +0545

    feat: remove subscription form

--- src/views/Subscription/index.tsx
+++ src/views/Subscription/index.tsx
@@ -1,11 +1,9 @@
-import SubscriptionForm from '@Components/Subscription/Form/SubscriptionForm';
 import SubscriptionTable from '@Components/Subscription/Table/SubscriptionTable';
 
 function Subscription() {
   return (
     <div className="grid h-full w-full grid-cols-[60%_40%] gap-4 p-8">
       <SubscriptionTable />
-      <SubscriptionForm />
     </div>
   );
 }
