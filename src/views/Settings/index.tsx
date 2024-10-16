544c9c4c99429fdab6deb55ecf6d5387c127f21e
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-16 07:50:48 +0545

    feat: add navigation and generalinfo

--- src/views/Settings/index.tsx
+++ src/views/Settings/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+// import GeneralInfo from '@Components/AccountSettings/GeneralInfo';
+import GeneralInfoForm from '@Components/AccountSettings/GeneralInfo/Form/GeneralInfoForm';
+import Navigation from '@Components/Navigation';
 
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <div className="h-full w-full ">
+      <Navigation headerName="settings" />
+      <div className="flex h-full w-full flex-col items-center justify-center bg-[#f2f2f2] p-6">
+        <GeneralInfoForm />
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
