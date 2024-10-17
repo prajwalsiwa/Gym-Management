a3be683f8fc7f1e7895736f32b0db32bf85c5cee
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-17 07:44:26 +0545

    feat: add children prop

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,17 @@
+import { ReactNode } from 'react';
+
 interface NavigationPropsTypes {
-  headerName: string;
+  headerName?: string;
+  children?: ReactNode;
 }
 
-function Navigation({ headerName }: NavigationPropsTypes) {
+function Navigation({ headerName, children }: NavigationPropsTypes) {
   return (
-    <div className="h-14 bg-blue-800">
-      <h4 className="flex h-full items-center px-8 text-white">{headerName}</h4>
+    <div className="grid h-14 w-full grid-cols-[40%_60%] items-center justify-between bg-gradient-to-r from-gray-800 to-gray-400 px-8 py-2">
+      <h4 className="flex h-full w-full items-center justify-start  text-white">
+        {headerName}
+      </h4>
+      <div className="flex w-full justify-between">{children}</div>
     </div>
   );
 }
