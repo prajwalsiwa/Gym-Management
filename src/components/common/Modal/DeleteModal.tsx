f8026c8ac1027e6d93d8feabb2c36b0d04ea0e3f
prajwalsiwa <prajwal.siwa1@gmail.com>
2024-10-20 21:41:39 +0545

    feat: create deleteModal ui

--- src/components/common/Modal/DeleteModal.tsx
+++ src/components/common/Modal/DeleteModal.tsx
@@ -1,5 +1,50 @@
-function DeleteModal() {
-  return <div>DeleteModal</div>;
+import { Button } from '@Components/RadixComponents/Button';
+import Modal from '.';
+
+interface deleteModalProps {
+  header: string;
+  subheader: string;
+  onDeleteClick: () => void;
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+function DeleteModal({
+  header,
+  onDeleteClick,
+  isOpen,
+  subheader,
+  handleClose,
+}: deleteModalProps) {
+  return (
+    <Modal
+      title={header}
+      subtitle={subheader}
+      show={isOpen}
+      className="j flex !w-[30rem] flex-col gap-4  "
+      onClose={handleClose}
+      headerClassName="w-full pl-4"
+    >
+      <div className="flex w-full flex-col items-center justify-center gap-4">
+        <div className="flex w-full gap-4">
+          <Button
+            className="text-md h-11 w-full rounded-2xl bg-gray-600 hover:bg-gray-700"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="text-md h-11 w-full rounded-2xl bg-red-600 hover:bg-red-700"
+            onClick={onDeleteClick}
+            leftIcon="delete"
+            iconClassname="!text-xl"
+          >
+            Delete
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
 }
 
 export default DeleteModal;
