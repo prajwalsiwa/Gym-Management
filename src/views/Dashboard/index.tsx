ac8d3b068c27a46ba4c46aaab585e100f3f45219
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-01-13 14:13:56 +0545

    feat: add navbar

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,7 +1,11 @@
+import Navbar from '@Components/Navbar/Navbar';
+
 export default function Dashboard() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <h2>Hello Dashboard</h2>
+    <div className="flex  h-full w-full justify-center p-4">
+      <div className="h-fit">
+        <Navbar />
+      </div>
     </div>
   );
 }
