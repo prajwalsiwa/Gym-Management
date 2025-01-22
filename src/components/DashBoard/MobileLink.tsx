446a2a9b315e75dfd98cc4b0290f3c805aeb6a24
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-01-22 15:40:56 +0545

    feat: create mobile link component

---
+++ src/components/DashBoard/MobileLink.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@Components/RadixComponents/Button';
+import React from 'react';
+
+function MobileLink() {
+  return (
+    <div className=" flex h-full  w-full flex-col justify-between  gap-12 rounded-lg border bg-grey-700 p-4">
+      <div className="flex flex-col gap-2">
+        <span className="flex-wrap text-2xl leading-7 text-white ">
+          Download Mobile App
+        </span>
+        <span className="text-sm text-white sm:text-base md:text-lg">
+          Manage the business whenever and wherever you are
+        </span>
+      </div>
+      <div className="flex flex-col gap-2 bg-sky-600">
+        <Button className="w-full bg-white text-black hover:bg-grey-200">
+          Download Now
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default MobileLink;
